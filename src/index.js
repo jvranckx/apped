25b1bc05114243008bc0ad9fd6822a00de926cd4
VranckxJaimie <111240314+VranckxJaimie@users.noreply.github.com>
2022-08-15 08:41:48 +0200

    e

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={"https://VranckxJaimie.github.io/apped"}>
+    <BrowserRouter basename={"https://www.apped.be/apped"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
